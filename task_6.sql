fb7227392b9a5998736e02d5b8435bcaccaada3d
damitheswitch <charradiimad7@gmail.com>
2025-04-01 12:41:40 +0800

    ngl, copied this from github

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,11 @@
-INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+CREATE DATABASE IF NOT EXISTS alx_book_store
 
+-- Use the database passed as an argument
+USE alx_book_store;
+
+-- Insert multiple rows into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES 
+	(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+	(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+	(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
